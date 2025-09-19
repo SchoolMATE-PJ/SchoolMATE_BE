25d290051618812c8ef30204b325e90c3c319b4b
JunYong <chary8028@naver.com>
2025-09-19 13:55:03 +0900

    Docs: KAN-17 Product entity 수정

--- schoolmate/src/main/java/com/spring/schoolmate/entity/Product.java
+++ schoolmate/src/main/java/com/spring/schoolmate/entity/Product.java
@@ -15,30 +15,29 @@ public class Product {
 
   @Id
   @GeneratedValue(strategy = GenerationType.IDENTITY)
-  @Column(name = "product_id")
   private Integer productId; // DB의 내부 식별자로 사용되는 고유 번호
 
-  @Column(name = "product_code", length = 50, nullable = false, unique = true)
+  @Column(length = 50, nullable = false, unique = true)
   private String productCode; // 상품번호 (CO123, CS352 등)
 
-  @Column(name = "product_category", length = 100, nullable = false)
+  @Column(length = 100, nullable = false)
   private String productCategory; // 상품 카테고리
 
-  @Column(name = "product_name", length = 100, nullable = false)
+  @Column(length = 100, nullable = false)
   private String productName; // 상품명
 
-  @Column(name = "product_points", nullable = false)
+  @Column(nullable = false)
   private Integer productPoints; // 상품 포인트
 
-  @Column(name = "expiration_date", nullable = false)
+  @Column(nullable = false)
   private Date expirationDate; // 유효기간
 
-  @Column(name = "stock", nullable = false)
+  @Column(nullable = false)
   private Integer stock; // 재고 수량
 
-  @Column(name = "total_quantity", nullable = false)
+  @Column(nullable = false)
   private Integer totalQuantity; // 전체 수량
 
-  @Column(name = "registration_date", nullable = false)
+  @Column(nullable = false)
   private Date registrationDate; // 상품 등록 일자
 }
--- schoolmate/src/main/java/com/spring/schoolmate/entity/ProductExchange.java
+++ schoolmate/src/main/java/com/spring/schoolmate/entity/ProductExchange.java
@@ -15,7 +15,6 @@ public class ProductExchange {
 
   @Id
   @GeneratedValue(strategy = GenerationType.IDENTITY)
-  @Column(name = "productexchange_id")
   private Integer productExchangeId; // 교환 상품 아이디
 
   @ManyToOne(fetch = FetchType.LAZY)
@@ -26,12 +25,12 @@ public class ProductExchange {
   @JoinColumn(name = "product_id", nullable = false)
   private Product product; // 상품 엔터티
 
-  @Column(name = "exchange_date", nullable = false)
+  @Column(nullable = false)
   private Date exchangeDate; // 상품 교환 일자
 
-  @Column(name = "usage_date", nullable = true)
+  @Column(nullable = true)
   private Date usageDate; // 상품 사용 일자
 
-  @Column(name = "exchangecard_status", length = 20, nullable = true)
+  @Column(length = 20, nullable = true)
   private String exchangeCardStatus; // 교환 상품 사용 상태
 }

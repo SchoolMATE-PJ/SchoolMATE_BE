16c08dbcd3fc6c23d93d3cd66e77aeaa69c0c3cb
KingStone0113 <kingstone0113@gmail.com>
2025-09-19 14:48:09 +0900

    Feat :: Role 수정

--- schoolmate/src/main/java/com/spring/schoolmate/entity/Role.java
+++ schoolmate/src/main/java/com/spring/schoolmate/entity/Role.java
@@ -9,12 +9,15 @@ import lombok.Setter;
 @Setter
 public class Role {
 
+    // Role 고유 ID
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "role_id")
     private Integer roleId;
 
+    // 권한명ㄴ
     // Enumerated(EnumType.STRING) :: Enum 타입을 문자열 자체로 DB에 저장
     @Enumerated(EnumType.STRING)
-    @Column(nullable = false)
+    @Column(name = "role_name", nullable = false)
     private RoleType roleName;
 
     // RoleType Enum으로 정의

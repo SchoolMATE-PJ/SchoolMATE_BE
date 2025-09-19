075a68665e8ace6fb3694abb311fbe85fddba60b
JunYong <chary8028@naver.com>
2025-09-19 11:45:02 +0900

    Feat: KAN-21 eatphoto entity 구현

---
+++ schoolmate/src/main/java/com/spring/schoolmate/entity/VisionLabel.java
@@ -0,0 +1,25 @@
+package com.spring.schoolmate.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Builder
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "vision_labels")
+public class VisionLabel {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  @Column(name = "label_id")
+  private Integer labelId; // 라벨 고유 식별자
+
+  @Column(name = "label_name", length = 100, nullable = false)
+  private String labelName; // AI가 식별한 라벨명
+
+  @Column(name = "language_code", length = 5, nullable = true)
+  private String languageCode; // 라벨의 언어 코드
+}
